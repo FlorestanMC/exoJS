6d23378b615e211474ee5257226da2370037121d
Florestan Mellé-Cappellin <150163972+FlorestanMC@users.noreply.github.com>
2024-05-08 00:46:33 +0200

    14

--- JS_Racing_JS_exercices/script/ex_12.js
+++ JS_Racing_JS_exercices/script/ex_12.js
@@ -1,11 +1,12 @@
 window.addEventListener('load', function(){
+    // cibler le contenu voulu
     var content = document.getElementsByTagName("footer")[0].children[0];
 
-    
+    // On met dans le local storage l'url de la photo et on le nomme ex12_img
    localStorage.setItem("ex12_img","https://www.coding-academy.fr/wp-content/uploads/2017/10/CODING_LOGO_CMJN.png");
-   
+   // on régupère l'url et on la stock dans la constante img
    const img = localStorage.getItem("ex12_img");
-
+   // On la met à l'endroit voulu
    content.innerHTML = "<img src ='" + img + "' style='width:100%' >"
 
 });
--- JS_Racing_JS_exercices/script/ex_13.js
+++ JS_Racing_JS_exercices/script/ex_13.js
@@ -1,28 +1,24 @@
 window.addEventListener('load', function(){
+    // cibler le contenu voulu
     var content = document.getElementsByTagName("footer")[0].children[0];
-    
-    
-    
-    
-
+    // On récupère l'img depuis le localStorage
    const img = localStorage.getItem("ex12_img");
-
+    // On rajoute l'img au bon endroit
    content.innerHTML = "<img src ='" + img + "' style='width:100%' >";
-
+    // On cible la zone de l'img
    var zoneimg = document.getElementsByTagName("img")[0];
-
+    // Ecouteur d'évènement au click on enlève l'image et on recharge la page 
    zoneimg.addEventListener('click', function(){
 
    localStorage.removeItem("ex12_img");
    location.reload();
 
-
    })
-
+   // Lorsque la souris survole la zone de l'img la taille de l'image est réduit de 5% 
    zoneimg.addEventListener('mouseenter', function(){
     zoneimg.style.width = 95 + "%"
    })
-
+   // Lorsque la souris quitte la zone de l'img la taille de l'image reviens à 100%
    zoneimg.addEventListener('mouseout', function(){
     zoneimg.style.width = 100 + "%"
    })
