44e55110b1565c86c7ad7ca27408dda2a0c906fa
Florestan Mellé-Cappellin <150163972+FlorestanMC@users.noreply.github.com>
2024-05-07 18:34:27 +0200

    Update ex_12.js

--- JS_Racing_JS_exercices/script/ex_12.js
+++ JS_Racing_JS_exercices/script/ex_12.js
@@ -1,1 +1,11 @@
+window.addEventListener('load', function(){
+    var content = document.getElementsByTagName("footer")[0].children[0];
+
     
+   localStorage.setItem("url","https://www.coding-academy.fr/wp-content/uploads/2017/10/CODING_LOGO_CMJN.png");
+   
+   const img = localStorage.getItem("url");
+
+   content.innerHTML = "<img src ='" + img + "' style='width:100%' >"
+
+});
